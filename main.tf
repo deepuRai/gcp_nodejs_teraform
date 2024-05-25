82bc9d93d6a0ee93205623b0f30071acfd4d93ac
deepakasara <84222618+deepakasara@users.noreply.github.com>
2024-05-25 23:46:07 +0530

    initial code

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Create new storage bucket in the US multi-region
+# with coldline storage
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "deepak_test_bucket" {
+  name          = "test-new-bucket"
+  location      = "US"
+  storage_class = "COLDLINE"
+
+  uniform_bucket_level_access = true
+}
